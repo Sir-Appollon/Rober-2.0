578db2eb46a2d13235283397a706b968287543a8
Sir-Appollon <paul.emilejen@gmail.com>
2025-05-03 20:14:42 -0400

    upddate password script deluge

--- scripts/deluge_control.py
+++ scripts/deluge_control.py
@@ -1,9 +1,7 @@
 from deluge_client import DelugeRPCClient
 
 try:
-    with DelugeRPCClient(
-        "127.0.0.1", 58846, "localclient", "e0db9d7d51b2c62b7987031174607aa822f94bc9"
-    ) as client:
+    with DelugeRPCClient("127.0.0.1", 58846, "paul", "deluge") as client:
         print("✅ Connection to Deluge succeeded.")
         torrents = client.call("core.get_torrents_status", {}, ["name"])
         print("📦 Torrents:")
