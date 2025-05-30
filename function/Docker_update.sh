b255a40eeee5f363ad8605833dbf071d0d181905
Sir-Appollon <paul.emilejen@gmail.com>
2025-05-30 14:34:40 -0400

    docker up sh

--- function/Docker_update.sh
+++ function/Docker_update.sh
@@ -1,5 +1,5 @@
 # Pull updates if not skipped
 if [ "$1" != "--skip-pull" ]; then
     git pull origin main || { echo "[ERROR] Git pull failed"; exit 1; }
-    docker-compose pull || { echo "[ERROR] Docker-compose pull failed"; exit 1; }
+    docker-compose pull || { echo "[ERROR] Docker compose pull failed"; exit 1; }
 fi
