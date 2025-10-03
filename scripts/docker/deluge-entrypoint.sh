ae1330f7b8b08deee7845d65dd1d9f5bf360e4a3
Sir-Appollon <paul.emilejen@gmail.com>
2025-10-03 10:28:41 -0400

    clean up

--- scripts/docker/deluge-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "[DELUGE] Lancement du script Python de configuration..."
-python3 /scripts/docker/start_deluge_after_vpn.py
-
-echo "[DELUGE] Script terminé, lancement de Deluge..."
-exec /init
